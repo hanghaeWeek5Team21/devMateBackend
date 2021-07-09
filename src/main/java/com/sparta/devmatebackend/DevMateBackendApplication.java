9968d6535cc92d6b478be63cbf3696a3cb235f85
dohyung97022 <dohyung97022@gmail.com>
2021-07-09 15:30:58 +0900

    java 설치, 공동 구동 확인 단계

---
+++ src/main/java/com/sparta/devmatebackend/DevMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.devmatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevMateBackendApplication.class, args);
+    }
+
+}
