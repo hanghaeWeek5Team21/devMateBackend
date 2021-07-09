ffc7667d228613ae2c0b33e1499c592fd7b20be8
Tbread <itaebread@gmail.com>
2021-07-09 16:25:12 +0900

    댓글 작성/삭제/수정을 위한 클래스추가

--- src/main/java/com/sparta/devmatebackend/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/devmatebackend/dto/CommentRequestDto.java
@@ -1,4 +1,17 @@
 package com.sparta.devmatebackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentRequestDto {
+    private Long id;
+    private Long user_id;
+    private String username;
+    private String contents;
 }
--- src/main/java/com/sparta/devmatebackend/service/CommentService.java
+++ src/main/java/com/sparta/devmatebackend/service/CommentService.java
@@ -1,4 +1,33 @@
 package com.sparta.devmatebackend.service;
 
+import com.sparta.devmatebackend.dto.CommentPutRequestDto;
+import com.sparta.devmatebackend.dto.CommentRequestDto;
+import com.sparta.devmatebackend.models.Comment;
+import com.sparta.devmatebackend.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public Long update(Long id, CommentPutRequestDto commentPutRequestDto){
+        Comment comment = commentRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 댓글입니다.")
+        );
+        comment.update(commentPutRequestDto);
+        return comment.getId();
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Comment comment = commentRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 댓글입니다.")
+        );
+        commentRepository.delete(comment);
+    }
 }
