0fede9555195eb05938baa177f66ca13e0da4cca
dohyung97022 <dohyung97022@gmail.com>
2021-09-06 20:37:25 +0900

    refactor : commentController 에 ResponseEntity 적용

--- src/main/java/com/sparta/devmatebackend/controller/CommentController.java
+++ src/main/java/com/sparta/devmatebackend/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.sparta.devmatebackend.controller;
 import com.sparta.devmatebackend.dto.CommentPutRequestDto;
 import com.sparta.devmatebackend.dto.CommentRequestDto;
 import com.sparta.devmatebackend.dto.CommentResponseDto;
+import com.sparta.devmatebackend.dto.ResMesResultResponseDto;
 import com.sparta.devmatebackend.models.Comment;
 import com.sparta.devmatebackend.models.User;
 import com.sparta.devmatebackend.repository.CommentRepository;
@@ -10,9 +11,12 @@ import com.sparta.devmatebackend.repository.UserRepository;
 import com.sparta.devmatebackend.security.UserDetailsImpl;
 import com.sparta.devmatebackend.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @CrossOrigin(origins = {"${config.domain.full-name}"}, allowCredentials = "true")
@@ -25,48 +29,28 @@ public class CommentController {
     private final UserRepository userRepository;
 
     @PostMapping("api/comment")
-    // TODO : ResponseEntity 로 변경하기
-    public CommentResponseDto create(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentRequestDto commentRequestDto) {
-        CommentResponseDto commentResponseDto;
-        try {
-            commentService.create(commentRequestDto, userDetails);
-            commentResponseDto = new CommentResponseDto(true, "댓글이 작성되었습니다.");
-        } catch (Exception e) {
-            // TODO : exception 처리하는 globalExceptionController 로 처리방법 변경하기
-            commentResponseDto = new CommentResponseDto(false, e.getMessage());
-        }
-        return commentResponseDto;
+    public ResponseEntity<CommentResponseDto> create(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentRequestDto commentRequestDto) {
+        commentService.create(commentRequestDto, userDetails);
+        URI currentRequest = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+        return ResponseEntity.created(currentRequest).body(new CommentResponseDto(true, "댓글이 작성되었습니다."));
     }
 
     @DeleteMapping("api/comment/{id}")
-    public CommentResponseDto delete(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
-        CommentResponseDto commentResponseDto;
-        try {
-            commentService.delete(id, userDetails);
-            commentResponseDto = new CommentResponseDto(true, "댓글이 삭제되었습니다.");
-        } catch (Exception e) {
-            // TODO : exception 처리하는 globalExceptionController 로 처리방법 변경하기
-            commentResponseDto = new CommentResponseDto(false, e.getMessage());
-        }
-        return commentResponseDto;
+    public ResponseEntity<CommentResponseDto> delete(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) throws IllegalAccessException {
+        commentService.delete(id, userDetails);
+        return ResponseEntity.ok().body(new CommentResponseDto(true, "댓글이 삭제되었습니다."));
     }
 
     @PatchMapping("api/comment/{id}")
-    public CommentResponseDto update(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentPutRequestDto requestDto) {
-        CommentResponseDto commentResponseDto;
-        try {
-            commentService.update(id, requestDto, userDetails);
-            commentResponseDto = new CommentResponseDto(true, "댓글이 수정되었습니다.");
-        } catch (Exception e) {
-            // TODO : exception 처리하는 globalExceptionController 로 처리방법 변경하기
-            commentResponseDto = new CommentResponseDto(false, e.getMessage());
-        }
-        return commentResponseDto;
+    public ResponseEntity<CommentResponseDto> update(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentPutRequestDto requestDto) throws IllegalAccessException {
+        commentService.update(id, requestDto, userDetails);
+        return ResponseEntity.ok().body(new CommentResponseDto(true, "댓글이 수정되었습니다."));
     }
 
     @GetMapping("api/comment")
-    public List<Comment> getComment(@RequestParam(value = "user_id") Long user_id) {
+    public ResponseEntity<ResMesResultResponseDto> getComment(@RequestParam(value = "user_id") Long user_id) {
         User user = userRepository.getById(user_id);
-        return commentRepository.findAllByUserOrderByCreatedAtDesc(user);
+        List<Comment> commentsByUser = commentRepository.findAllByUserOrderByCreatedAtDesc(user);
+        return ResponseEntity.ok().body(new ResMesResultResponseDto(true, "댓글을 조회합니다.", commentsByUser));
     }
 }
