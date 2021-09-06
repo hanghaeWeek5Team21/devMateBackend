a134c838b09765a86b1f805e1c34b60ca77c5027
dohyung97022 <dohyung97022@gmail.com>
2021-09-06 23:41:25 +0900

    fix : Config 는 Setter 를 public 으로 열어줘야 동작합니다.

--- src/main/java/com/sparta/devmatebackend/config/DomainConfig.java
+++ src/main/java/com/sparta/devmatebackend/config/DomainConfig.java
@@ -1,15 +1,14 @@
 package com.sparta.devmatebackend.config;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration("domain")
 @ConfigurationProperties(prefix = "config.domain")
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@Setter
 public class DomainConfig {
-    private final String fullName;
+    private String fullName;
 }
