dcd360c4e20d5f1d903641aa45a7dffe9f38672c
dohyung97022 <dohyung97022@gmail.com>
2021-09-06 20:45:25 +0900

    refactor : FileUploadController 에 ResponseEntity 적용

--- src/main/java/com/sparta/devmatebackend/controller/FileUploadController.java
+++ src/main/java/com/sparta/devmatebackend/controller/FileUploadController.java
@@ -15,8 +15,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 
 
 @CrossOrigin(origins = {"${config.domain.full-name}"}, allowCredentials = "true")
@@ -39,48 +42,42 @@ public class FileUploadController {
     }
 
     @PostMapping("api/file/image")
-    public ResMesResultResponseDto handleImageUpload(@RequestParam("file") MultipartFile file) {
-        ResMesResultResponseDto resMesResultResponseDto;
-        try {
-            // 파일의 크기를 확인합니다.
-            if (storageService.fileOverSize(file, MAX_FILE_BYTES)) {
-                throw new FileOverMaxSizeException("파일이 512MB를 넘습니다.");
-            }
+    public ResponseEntity<ResMesResultResponseDto> handleImageUpload(@RequestParam("file") MultipartFile file) throws IOException {
 
-            // 파일을 저장합니다.
-            File savedFile = storageService.store(file);
-
-            // 파일의 모든 이름, 확장자, 위치를 가져옵니다.
-            String fileName = savedFile.getName();
-            String fileExtension = FileNameUtils.getExtensionFromFileName(savedFile.getName())
-                    .orElseThrow(() -> new FileNoExtensionException("파일에 확장자가 없습니다."));
-            String fileLocation = "./" + storageConfig.getLocation() + "/";
-
-            // 파일이 s3에 중복되는지 확인합니다.
-            while (s3Object.objectExists(fileName)) {
-                // 중복되면 파일명을 다르게 변경합니다.
-                fileName = StringUtils.generateRandomString() + fileExtension;
-                // 변경된 파일명을 적용합니다.
-                if (savedFile.renameTo(new File( fileLocation + fileName))) {
-                    savedFile = new File(fileLocation + fileName);
-                }else{
-                    throw new FileRenameException("파일의 이름을 바꾸지 못합니다.");
-                }
-            }
-
-            // 저장된 파일을 s3에 업로드합니다.
-            s3Object.upload(savedFile);
-
-            // 업로드된 파일 URL 을 받습니다.
-            String url = s3Object.getObjectUrl(savedFile);
-
-            resMesResultResponseDto = new ResMesResultResponseDto(true, "파일이 서버에 저장되었습니다.", url);
-
-        } catch (Exception e) {
-            resMesResultResponseDto = new ResMesResultResponseDto(false, e.getMessage(), null);
-            System.out.println("e.getMessage() = " + e.getMessage());
+        // 파일의 크기를 확인합니다.
+        if (storageService.fileOverSize(file, MAX_FILE_BYTES)) {
+            throw new FileOverMaxSizeException("파일이 512MB를 넘습니다.");
         }
 
-        return resMesResultResponseDto;
+        // 파일을 저장합니다.
+        File savedFile = storageService.store(file);
+
+        // 파일의 모든 이름, 확장자, 위치를 가져옵니다.
+        String fileName = savedFile.getName();
+        String fileExtension = FileNameUtils.getExtensionFromFileName(savedFile.getName())
+                .orElseThrow(() -> new FileNoExtensionException("파일에 확장자가 없습니다."));
+        String fileLocation = "./" + storageConfig.getLocation() + "/";
+
+        // 파일이 s3에 중복되는지 확인합니다.
+        while (s3Object.objectExists(fileName)) {
+            // 중복되면 파일명을 다르게 변경합니다.
+            fileName = StringUtils.generateRandomString() + fileExtension;
+            // 변경된 파일명을 적용합니다.
+            if (savedFile.renameTo(new File(fileLocation + fileName))) {
+                savedFile = new File(fileLocation + fileName);
+            } else {
+                throw new FileRenameException("파일의 이름을 바꾸지 못합니다.");
+            }
+        }
+
+        // 저장된 파일을 s3에 업로드합니다.
+        s3Object.upload(savedFile);
+
+        // 업로드된 파일 URL 을 받습니다.
+        String s3ObjectUrl = s3Object.getObjectUrl(savedFile);
+
+        URI currentRequest = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+
+        return ResponseEntity.created(currentRequest).body(new ResMesResultResponseDto(true, "파일이 서버에 저장되었습니다.", s3ObjectUrl));
     }
 }
