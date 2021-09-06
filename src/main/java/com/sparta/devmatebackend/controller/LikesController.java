ad3fa50db6f8a9dd8ce25506c3abe446239159ab
dohyung97022 <dohyung97022@gmail.com>
2021-09-06 21:47:56 +0900

    refactor : LikesController 에 ResponseEntity 적용

--- src/main/java/com/sparta/devmatebackend/controller/LikesController.java
+++ src/main/java/com/sparta/devmatebackend/controller/LikesController.java
@@ -2,44 +2,33 @@ package com.sparta.devmatebackend.controller;
 
 import com.sparta.devmatebackend.dto.CommentResponseDto;
 import com.sparta.devmatebackend.dto.LikePutRequestDto;
-import com.sparta.devmatebackend.repository.LikesRepository;
 import com.sparta.devmatebackend.security.UserDetailsImpl;
 import com.sparta.devmatebackend.service.LikesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @CrossOrigin(origins = {"${config.domain.full-name}"}, allowCredentials = "true")
 @RestController
 @RequiredArgsConstructor
 public class LikesController {
 
-    private final LikesRepository likesRepository;
     private final LikesService likesService;
 
     @PostMapping("api/likes")
-    // TODO : ResponseEntity 로 변경하기
-    public CommentResponseDto create(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody LikePutRequestDto likePutRequestDto) {
-        CommentResponseDto commentResponseDto;
-        try {
-            likesService.create(likePutRequestDto, userDetails);
-            commentResponseDto = new CommentResponseDto(true, "좋아요가 작성되었습니다.");
-        } catch (Exception e) {
-            commentResponseDto = new CommentResponseDto(false, e.getMessage());
-        }
-        return commentResponseDto;
+    public ResponseEntity<CommentResponseDto> create(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody LikePutRequestDto likePutRequestDto) {
+        likesService.create(likePutRequestDto, userDetails);
+        URI currentRequest = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+        return ResponseEntity.created(currentRequest).body(new CommentResponseDto(true, "좋아요가 작성되었습니다."));
     }
 
     @DeleteMapping("api/likes/{id}")
-    // TODO : ResponseEntity 로 변경하기
-    public CommentResponseDto delete(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
-        CommentResponseDto commentResponseDto;
-        try {
-            likesService.delete(id, userDetails);
-            commentResponseDto = new CommentResponseDto(true, "좋아요가 삭제되었습니다.");
-        } catch (Exception e) {
-            commentResponseDto = new CommentResponseDto(false, e.getMessage());
-        }
-        return commentResponseDto;
+    public ResponseEntity<CommentResponseDto> delete(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) throws IllegalAccessException {
+        likesService.delete(id, userDetails);
+        return ResponseEntity.ok().body(new CommentResponseDto(true, "좋아요가 삭제되었습니다."));
     }
 }
